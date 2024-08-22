090bb286bdf3936d14652a250722d01cda58894f
Juanes <trianajuan95@gmail.com>
2024-08-21 16:33:53 -0800

    Update002

---
+++ js/about.js
@@ -0,0 +1,7 @@
+let FirstName = "Juan";
+let LastName = "Triana";
+let programName = "Full Stack Web Developer";
+
+document.getElementById("name").textContent = FirstName + "" + LastName;
+document.getElementById("name1").textContent = FirstName + "" + LastName;
+document.getElementById("program").textContent = programName;
